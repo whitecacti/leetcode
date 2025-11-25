3b9c370252bb9855fb424ac20022986596fe1365
default <C704839B-921B-45A6-AA05-5644898FAD35@no.no>
2025-11-25 05:54:25 +0000

    9

---
+++ 9/with_string.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def isPalindrome(x: int) -> bool:
+    return str(x) == str(x)[::-1]
+
+
+if __name__ == "__main__":
+    print(isPalindrome(121))
+    print(isPalindrome(-121))
+    print(isPalindrome(10))
+    
